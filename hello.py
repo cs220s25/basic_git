484e75406f039ede0d098d7bd5cfc3a4d0f2f086
Ben Coleman <colemanb@moravian.edu>
2024-02-08 07:23:20 -0500

    refactor code into a function

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-name = input('What is your name? ')
-print('Hello, {}!'.format(name))
+
+def say_hello(name):
+    print('Hello, {}!'.format(name))
+
+
+if __name__ == '__main__':
+    name = input('What is your name? ')
+    say_hello(name)
 
